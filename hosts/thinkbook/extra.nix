ad5c1a4984172269de5c5a43f5ef2e565880cde2
padd1er <the-coder-p@proton.me>
2024-08-04 12:45:58 +0200

    config: switch extras

--- hosts/thinkbook/extra.nix
+++ hosts/thinkbook/extra.nix
@@ -7,21 +7,23 @@
     # ../../nix-modules/bundles/gnome-setup.nix
     # ../../nix-modules/bundles/kde-setup.nix
     # and this below are custom
-    ../../nix-modules/bundles/general-setup.nix
-    ../../nix-modules/features/app/default-gui.nix
+    #../../nix-modules/bundles/general-setup.nix
+    #../../nix-modules/features/app/default-gui.nix
+    ../../nix-modules/bundles/xfce-setup.nix
     # ../../nix-modules/features/sys/de-cosmic.nix
-    ../../nix-modules/features/sys/wm-hyprland.nix
+    # ../../nix-modules/features/sys/wm-hyprland.nix
   ];
 
   # sys-de-cosmic.enable = true;
-  sys-wm-hyprland.enable = true;
+  # sys-wm-hyprland.enable = true;
 
-  # environment.systemPackages = [
+  environment.systemPackages = [
+    pkgs.firefox
   #   pkgs.nix-ld-rs
   #   pkgs.nix-diff
   #   pkgs.nix-index
   #   pkgs.nix-health
-  # ];
+  ];
 
 }
 
