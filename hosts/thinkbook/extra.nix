2a7501082faa44fc5c89c124ba655da5d71541fc
padd1er <the-coder-p@proton.me>
2024-08-05 14:03:16 +0200

    config: extra enable modules

--- hosts/thinkbook/extra.nix
+++ hosts/thinkbook/extra.nix
@@ -11,6 +11,12 @@
     ../../nix-modules/bundles/setup-xfce.nix
   ];
 
+  sys-nixos.enable = lib.mkForce true;
+  sys-system.enable = lib.mkForce true;
+  sys-systemd.enable = lib.mkForce true;
+  sys-main-user.enable = lib.mkForce true;
+
+
   environment.systemPackages = [
     #   pkgs.nix-ld-rs
     #   pkgs.nix-diff
