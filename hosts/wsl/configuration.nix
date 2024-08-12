1dcb77bf164fa4b4596d6adb18766e85fba99c8e
padd1er <the-coder-p@proton.me>
2024-08-12 13:16:29 +0200

    config: wsl set default settings

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -13,12 +13,17 @@
 {
   imports = [
     inputs.nixos-wsl.nixosModules.default
+    ../../nix-modules/bundles/default.nix
   ];
 
   wsl.enable = true;
   wsl.defaultUser = "nixos";
 
   networking.hostName = "wsl-nixos";
+
+  sys-nixos.enable = lib.mkForce true;
+  sys-system.enable = lib.mkForce true;
+  sys-systemd.enable = lib.mkDefault true;
 
   system.stateVersion = "24.05";
 }
