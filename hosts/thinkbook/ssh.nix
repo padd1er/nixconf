da251e1d4ce8bb53091ba6a37443bdfe31103fd7
padd1er <the-coder-p@proton.me>
2024-08-12 11:58:05 +0200

    refactor: fix the key itself and set a proper permissions to the file

--- hosts/thinkbook/ssh.nix
+++ hosts/thinkbook/ssh.nix
@@ -9,9 +9,10 @@ let
   sysMainUserModule = ../../nix-modules/features/sys/main-user.nix;
   sysMainUser = import sysMainUserModule { inherit pkgs lib config inputs; };
 
-  userName = sysMainUser.username;
+  userName = config.sys-main-user.username;
   homeDir = "/home/${userName}";
   secretsFile = ../../secrets.yaml;
+  keyFile = "${homeDir}/.ssh/${userName}_at_thinkbook";
 in
 {
   imports = [
@@ -22,11 +23,13 @@ in
           userName='"${userName}"'
           secretsFile='"${secretsFile}"'
           homeDir='"${homeDir}"'
+          keyFile='"${keyFile}"'
           key=$(SOPS_AGE_KEY_FILE=/tmp/nix-age.txt ${pkgs.sops}/bin/sops --decrypt --extract "['thinkbook_key']" ${secretsFile})
       if [ ! -d ${homeDir}/.ssh ]; then
         runuser -l ${userName} -c "mkdir -p ${homeDir}/.ssh"
       fi
-      runuser -l ${userName} -c "printf '%s\n' \"$key\" > ${homeDir}/.ssh/${userName}_at_thinkbook"
+      runuser -l ${userName} -c "echo \"$key\" > ${keyFile}"
+      runuser -l ${userName} -c "chmod 600 ${keyFile}"
     '';
   };
 }
