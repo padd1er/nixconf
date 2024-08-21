2d2ecf78e3e72e52781953a60aa260b03ddb18ab
padd1er <the-coder-p@proton.me>
2024-08-21 10:21:08 +0200

    refactor: sorting

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -1,13 +1,14 @@
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
 
-{ config
-, lib
-, pkgs
-, inputs
-, outputs
-, system
-, ...
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  outputs,
+  system,
+  ...
 }:
 
 {
@@ -31,4 +32,3 @@
 
   system.stateVersion = "24.05";
 }
-
