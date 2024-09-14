4427475bd5a56916f05de2293cd278ddcf8bc8fa
padd1er <the-coder-p@proton.me>
2024-09-14 14:18:54 +0200

    feat: golang flake shell

---
+++ tmp/golang-develop/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Golang dev shell flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+
+        name = "Golang dev shell";
+
+        packages = [ pkgs.go ];
+
+        shellHook = '''';
+      };
+
+    };
+
+}
