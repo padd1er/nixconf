fd7254448ed8cd01fae8d1e7d28f1a04bab04e16
padd1er <the-coder-p@proton.me>
2024-09-14 13:39:48 +0200

    config: fix typo

--- hosts/common/users/padd1er/default.nix
+++ hosts/common/users/padd1er/default.nix
@@ -30,43 +30,61 @@ in
     shell = pkgs.fish;
   };
 
-  system.userAactivationScripts.cloneRepo = {
-    text = ''
-      secretsFile='"${secretsFile}"'
-      homeDir='"${homeDir}"'
-      token=$(SOPS_AGE_KEY_FILE=/tmp/nix-age.txt ${pkgs.sops}/bin/sops --decrypt --extract "['gitlab_token']" ${secretsFile})
-      user=$(SOPS_AGE_KEY_FILE=/tmp/nix-age.txt ${pkgs.sops}/bin/sops --decrypt --extract "['gitlab_user']" ${secretsFile})
-      if [ ! -d ${homeDir}/.dotfiles ]; then
-        # Clone the repository
-        runuser -l ${userName} -c "git clone https://$user:$token@gitlab.com/padd1er_linux/dotfiles.git ${homeDir}/.dotfiles"
+  system.userActivationScripts = {
+    cloneRepo = {
+      text = ''
+        secretsFile='"${secretsFile}"'
+        homeDir='"${homeDir}"'
+        token=$(SOPS_AGE_KEY_FILE=/tmp/nix-age.txt ${pkgs.sops}/bin/sops --decrypt --extract "['gitlab_token']" ${secretsFile})
+        user=$(SOPS_AGE_KEY_FILE=/tmp/nix-age.txt ${pkgs.sops}/bin/sops --decrypt --extract "['gitlab_user']" ${secretsFile})
+        if [ ! -d ${homeDir}/.dotfiles ]; then
+          # Clone the repository
+          runuser -l ${userName} -c "git clone https://$user:$token@gitlab.com/padd1er_linux/dotfiles.git ${homeDir}/.dotfiles"
 
-        # Add the submodule section to the .git/config file
-        runuser -l ${userName} -c "git -C ${homeDir}/.dotfiles config --add submodule.stow/nvim/.config/nvim.url https://$user:$token@gitlab.com/padd1er_linux/config-neovim.git"
+          # Add the submodule section to the .git/config file
+          runuser -l ${userName} -c "git -C ${homeDir}/.dotfiles config --add submodule.stow/nvim/.config/nvim.url https://$user:$token@gitlab.com/padd1er_linux/config-neovim.git"
 
-        # Initialize and update the submodule
-        runuser -l ${userName} -c "git -C ${homeDir}/.dotfiles submodule update --init --recursive"
+          # Initialize and update the submodule
+          runuser -l ${userName} -c "git -C ${homeDir}/.dotfiles submodule update --init --recursive"
 
-        # Stow dotfiles
-        runuser -l ${userName} -c "cd ${homeDir}/.dotfiles && make stowall"
+          # Stow dotfiles
+          runuser -l ${userName} -c "cd ${homeDir}/.dotfiles && make stowall"
 
-        # Post commands
-        runuser -l ${userName} -c "bat cache --build"
-      fi
-    '';
+          # Post commands
+          runuser -l ${userName} -c "bat cache --build"
+        fi
+      '';
+    };
+
+    setSshKey = {
+      text = ''
+        userName='"${userName}"'
+        secretsFile='"${secretsFile}"'
+        homeDir='"${homeDir}"'
+        keyFile='"${keyFile}"'
+        key=$(SOPS_AGE_KEY_FILE=/tmp/nix-age.txt ${pkgs.sops}/bin/sops --decrypt --extract "['padd1er_key']" ${secretsFile})
+        if [ ! -d ${homeDir}/.ssh ]; then
+          runuser -l ${userName} -c "mkdir -p ${homeDir}/.ssh"
+        fi
+        runuser -l ${userName} -c "echo \"$key\" > ${keyFile}"
+        runuser -l ${userName} -c "chmod 600 ${keyFile}"
+      '';
+    };
+
   };
 
-  system.userAactivationScripts.setSshKey = {
-    text = ''
-      userName='"${userName}"'
-      secretsFile='"${secretsFile}"'
-      homeDir='"${homeDir}"'
-      keyFile='"${keyFile}"'
-      key=$(SOPS_AGE_KEY_FILE=/tmp/nix-age.txt ${pkgs.sops}/bin/sops --decrypt --extract "['padd1er_key']" ${secretsFile})
-      if [ ! -d ${homeDir}/.ssh ]; then
-        runuser -l ${userName} -c "mkdir -p ${homeDir}/.ssh"
-      fi
-      runuser -l ${userName} -c "echo \"$key\" > ${keyFile}"
-      runuser -l ${userName} -c "chmod 600 ${keyFile}"
-    '';
-  };
+  # system.userAactivationScripts.setSshKey = {
+  #   text = ''
+  #     userName='"${userName}"'
+  #     secretsFile='"${secretsFile}"'
+  #     homeDir='"${homeDir}"'
+  #     keyFile='"${keyFile}"'
+  #     key=$(SOPS_AGE_KEY_FILE=/tmp/nix-age.txt ${pkgs.sops}/bin/sops --decrypt --extract "['padd1er_key']" ${secretsFile})
+  #     if [ ! -d ${homeDir}/.ssh ]; then
+  #       runuser -l ${userName} -c "mkdir -p ${homeDir}/.ssh"
+  #     fi
+  #     runuser -l ${userName} -c "echo \"$key\" > ${keyFile}"
+  #     runuser -l ${userName} -c "chmod 600 ${keyFile}"
+  #   '';
+  # };
 }
