1a6a04f0ffbdbf89e1a1e52437160d3adca115e1
padd1er <the-coder-p@proton.me>
2025-10-08 22:47:58 +0200

    fix: qmk is broken in unstable

--- hosts/common/optional/apps/qmk.nix
+++ hosts/common/optional/apps/qmk.nix
@@ -1,11 +1,15 @@
 {
+  inputs,
   pkgs,
   ...
 }:
 
 {
   environment.systemPackages = with pkgs; [
-    qmk
+    inputs.nixpkgs-stable.legacyPackages."${pkgs.system}".qmk
+    # TODO: Compatibility with CMake < 3.5 has been removed from CMake - unstable
+    # NOTE: https://github.com/NixOS/nixpkgs/issues/445447
+    # qmk
     qmk-udev-rules
     qmk_hid
   ];
