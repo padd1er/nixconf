4005cc4503b3cb7a1bad73d99b0b2997db02de57
padd1er <the-coder-p@proton.me>
2025-10-10 00:24:53 +0200

    refactor: working yubikey config

--- hosts/common/optional/apps/yubikey.nix
+++ hosts/common/optional/apps/yubikey.nix
@@ -1,70 +1,58 @@
 {
   pkgs,
+  config,
   ...
 }:
 
+let
+  userName = config.hostSpec.primaryUser.name;
+in
 {
-
-  imports = [
-  ];
-
   environment.systemPackages = with pkgs; [
     yubioath-flutter
     yubikey-manager
     pam_u2f
-    # pcsclite
+    yubikey-personalization
+    pamtester
+    age-plugin-yubikey
   ];
 
   services = {
     pcscd.enable = true;
     udev.packages = [ pkgs.yubikey-personalization ];
     yubikey-agent.enable = true;
-
-    # login.u2fAuth = true;
-    # sudo = {
-    #   u2fAuth = true;
-    #   # sshAgentAuth = true; # Use SSH_AUTH_SOCK for sudo
-    # };
   };
 
-  # TODO: deal with this
-  # security.pam = {
-  #   services = {
-  #     login = {
-  #       u2fAuth = true;
-  #       unixAuth = true;
-  #     };
-  #     sudo = {
-  #       u2fAuth = true;
-  #       unixAuth = true;
-  #     };
-  #     lightdm = {
-  #       u2fAuth = true;
-  #       unixAuth = true;
-  #     };
-  #   };
-  #   # sshAgentAuth.enable = true;
-  #   u2f = {
-  #     enable = true;
-  #     settings = {
-  #       cue = true;
-  #       authFile = "$HOME/.ssh/u2f_keys";
-  #     };
-  #   };
-  # };
+  sops.secrets.yubikey_u2f_keys = {
+    sopsFile = config.sops.defaultSopsFile;
+  };
 
-  # yubikey login / sudo
-  # security.pam = lib.optionalAttrs pkgs.stdenv.isLinux {
-  #   sshAgentAuth.enable = true;
-  #   u2f = {
-  #     enable = true;
-  #     settings = {
-  #       cue = true; # Tells user they need to press the button
-  #       authFile = "${homeDirectory}/.config/Yubico/u2f_keys";
-  #     };
-  #   };
+  sops.templates."u2f_keys" = {
+    content = ''
+      ${userName}:${config.sops.placeholder.yubikey_u2f_keys}
+    '';
+    mode = "0444";
+  };
 
-  # services.pcscd.enable = true;
-  # services.udev.packages = [ pkgs.yubikey-personalization ];
-  # services.yubikey-agent.enable = true;
+  security.pam = {
+    u2f = {
+      enable = true;
+      settings = {
+        cue = true;
+        authfile = config.sops.templates."u2f_keys".path;
+        origin = "pam://nixos-yubikey";
+        appid = "pam://nixos-yubikey";
+      };
+      control = "sufficient";
+    };
+    services = {
+      login.u2fAuth = true;
+      sudo.u2fAuth = true;
+      sudo-i.u2fAuth = true;
+      lightdm.u2fAuth = true;
+      polkit-1.u2fAuth = true;
+      gdm.u2fAuth = true;
+      sddm.u2fAuth = true;
+    };
+  };
 }
