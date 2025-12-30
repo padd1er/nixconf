eb6e01beab3da73391d3f4b841fcc53a0cdc757e
padd1er <the-coder-p@proton.me>
2025-12-30 11:26:36 +0100

    refactor: cleanup optional gui

--- hosts/common/optional/apps/all-gui.nix
+++ hosts/common/optional/apps/all-gui.nix
@@ -6,32 +6,24 @@
 
 {
   environment.systemPackages = with pkgs; [
-    # alacritty
     amberol
     bibata-cursors
     bitwarden-desktop
     # firefox
-    # flameshot
-    # gradia
-    # shutter
     ghostty
     google-chrome
     inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
-    # keepassxc
+    keepassxc
     localsend
     mpv
     nomacs
     obs-studio
     papirus-icon-theme
-    rustdesk-flutter # NOTE: this is instead lf default rustdesk which is at 1.2.3 version and fails to build due to rust 1.80.0 https://github.com/NixOS/nixpkgs/issues/332957
-    # NOTE: this is due to qtwebengine5-15 EOL/deprecation
-    # inputs.nixpkgs-stable.legacyPackages."${pkgs.stdenv.hostPlatform.system}".seafile-client
+    # rustdesk
+    rustdesk-flutter
     seafile-client
     slack
     spotify
     telegram-desktop
   ];
-
-  # catppuccin.flavor = "mocha";
-  # catppuccin.enable = true;
 }
