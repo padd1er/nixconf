e06a3bf59bad578164bd6306993ce88cae82bae2
padd1er <the-coder-p@proton.me>
2026-01-03 13:10:34 +0100

    refactor(torrent): cleanup

--- hosts/nixos/torrent/default.nix
+++ hosts/nixos/torrent/default.nix
@@ -35,10 +35,8 @@
       "hosts/common/optional/apps/wezterm.nix"
       "hosts/common/optional/apps/winboat.nix"
       "hosts/common/optional/apps/yubikey.nix"
-      # # "hosts/common/optional/user-environments/xfce.nix"
       "hosts/common/optional/user-environments/greetd.nix"
       "hosts/common/optional/user-environments/niri.nix"
-      # # "hosts/common/optional/user-environments/hypr.nix"
     ])
   ];
 
