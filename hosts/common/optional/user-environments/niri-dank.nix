d3ccd9691ab970cb86eea23953fce22f0423f897
padd1er <the-coder-p@proton.me>
2026-01-07 14:15:20 +0100

    refactor(niri-dank): attempt to fix swayidle

--- hosts/common/optional/user-environments/niri-dank.nix
+++ hosts/common/optional/user-environments/niri-dank.nix
@@ -49,6 +49,7 @@
     swayidle = {
       description = "Idle Service";
       path = with pkgs; [
+        dms-shell
         swayidle
         niri
       ];
@@ -56,11 +57,11 @@
         Type = "simple";
         ExecStart = ''
           ${pkgs.swayidle}/bin/swayidle -w \
-            timeout 600 'dms ipc call lock lock' \
+            timeout 600 '${pkgs.dms-shell}/bin/dms ipc call lock lock' \
             timeout 900 '${pkgs.niri}/bin/niri msg action power-off-monitors' \
               resume '${pkgs.niri}/bin/niri msg action power-on-monitors' \
             timeout 1800 'systemctl suspend' \
-            before-sleep 'dms ipc call lock lock'
+            before-sleep '${pkgs.dms-shell}/bin/dms ipc call lock lock'
         '';
         Restart = "on-failure";
       };
