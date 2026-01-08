8ba1e8263390e375f746dc856566c34f095dddfd
padd1er <the-coder-p@proton.me>
2026-01-08 21:21:21 +0100

    refactor(thinkbook): add chatting

--- hosts/nixos/thinkbook/default.nix
+++ hosts/nixos/thinkbook/default.nix
@@ -30,6 +30,7 @@
       "hosts/common/optional/apps/all-gui.nix"
       "hosts/common/optional/apps/qmk.nix"
       "hosts/common/optional/apps/gaming.nix"
+      "hosts/common/optional/apps/chatting.nix"
       "hosts/common/optional/apps/libreoffice.nix"
       "hosts/common/optional/apps/wezterm.nix"
       "hosts/common/optional/apps/piper-tts.nix"
