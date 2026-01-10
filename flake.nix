3fed9c19dea4eebdea988fe4cdd8a19dc7f6617c
padd1er <the-coder-p@proton.me>
2026-01-10 13:30:15 +0100

    refactor(flake): add dms flake url

--- flake.nix
+++ flake.nix
@@ -60,6 +60,11 @@
 
     scopebuddy.url = "github:HikariKnight/ScopeBuddy";
 
+    dms = {
+      url = "github:AvengeMedia/DankMaterialShell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # dgop = {
     #   url = "github:AvengeMedia/dgop";
     #   inputs.nixpkgs.follows = "nixpkgs";
