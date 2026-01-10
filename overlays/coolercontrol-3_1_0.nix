ad0bb85e925559ddd11fef8e5a55c76c04d28d3c
padd1er <the-coder-p@proton.me>
2026-01-10 13:30:00 +0100

    refactor(overlays): add coolercontrol possible overlay

--- overlays/coolercontrol-3_1_0.nix
+++ overlays/coolercontrol-3_1_0.nix
@@ -1,34 +1,49 @@
 final: prev:
-
 let
   version = "3.1.0";
-
   src = prev.fetchFromGitLab {
     owner = "coolercontrol";
     repo = "coolercontrol";
     rev = version;
-    hash = prev.lib.fakeHash;
-    # hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    hash = "sha256-cdJEiWALqSBF8egd1uSKJO/LkVkwYER5y/7R9lPF/D4=";
   };
 in
 {
-  coolercontrol = prev.coolercontrol.overrideScope' (
-    self: super: {
-      coolercontrol-ui-data = super.coolercontrol-ui-data.override {
+  coolercontrol = prev.coolercontrol // {
+    coolercontrold = prev.coolercontrol.coolercontrold.overrideAttrs (
+      finalAttrs: prevAttrs: {
         inherit version src;
-        npmDepsHash = prev.lib.fakeHash;
-        # npmDepsHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-      };
+        sourceRoot = "${src.name}/coolercontrold";
+        cargoHash = "sha256-8piv20iTwEWvVF+zDdtynW7bExW3FY7J3T/+MMb+wEQ=";
 
-      coolercontrold = super.coolercontrold.override {
-        inherit version src;
-        cargoHash = prev.lib.fakeHash;
-        # cargoHash = "sha256-CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC=";
-      };
+        nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ final.protobuf ];
 
-      coolercontrol-gui = super.coolercontrol-gui.override {
+        # Must override cargoDeps to use the new source
+        cargoDeps = final.rustPlatform.fetchCargoVendor {
+          inherit src;
+          sourceRoot = "${src.name}/coolercontrold";
+          name = "${finalAttrs.pname}-${version}";
+          hash = finalAttrs.cargoHash;
+        };
+      }
+    );
+    coolercontrol-ui-data = prev.coolercontrol.coolercontrol-ui-data.overrideAttrs (
+      finalAttrs: prevAttrs: {
         inherit version src;
-      };
-    }
-  );
+        sourceRoot = "${src.name}/coolercontrol-ui";
+        npmDepsHash = "sha256-v5YMMcSFjaCsjKaAv58v4vmXqAvxHDhhdj7tvvIjDnc=";
+
+        # Must override npmDeps to use the new source
+        npmDeps = final.fetchNpmDeps {
+          inherit src;
+          sourceRoot = "${src.name}/coolercontrol-ui";
+          name = "${finalAttrs.pname}-${version}-npm-deps";
+          hash = finalAttrs.npmDepsHash;
+        };
+      }
+    );
+    coolercontrol-gui = prev.coolercontrol.coolercontrol-gui.overrideAttrs (_: {
+      inherit version src;
+    });
+  };
 }
--- overlays/default.nix
+++ overlays/default.nix
@@ -4,4 +4,5 @@
   nirius = import ./nirius.nix;
   asrock-nct6683 = import ./asrock-nct6683.nix;
   easyeffects = import ./easyeffects.nix;
+  coolercontrol-3_1_0 = import ./coolercontrol-3_1_0.nix;
 }
